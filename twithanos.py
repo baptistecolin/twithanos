c0b288906628b3302728c17f1f4ae9feec3b9020
Baptiste Colin <baptiste.colin@outlook.fr>
2019-12-07 15:50:52 +0100

    removal of tests

--- twithanos.py
+++ twithanos.py
@@ -13,14 +13,8 @@ def softblock(user_id, screen_name):
     api.CreateBlock(user_id=user_id, screen_name=screen_name)
     api.DestroyBlock(user_id=user_id, screen_name=screen_name)
 
-sgt_bot_id = 867280041339744257
-sgt_bot_name = "singletau_bot"
-
 followers = api.GetFollowers()
 followings = api.GetFriends()
-
-#followers  = [ "a", "b", "c", "d", "e", "f", "g" ]
-#followings = [ "a", "b", "r", "t", "u", "w", "x", "y", "z" ]
 
 print(str(len(followers)) + " followers")
 print(str(len(followings)) + " followings")
@@ -33,11 +27,11 @@ non_mutuals = list(set(followers) - set(mutuals))
 
 assert (len(non_mutuals) == len(followers) - len (mutuals))
 
-print ("computing list of people who will get blocked ...")
+print ("computing list of people who will get softblocked ...")
 
 nb = min(int(len(followers)/2), len(followers) - len(mutuals))
 
-print(str(nb) + " people will be blocked")
+print(str(nb) + " people will be softblocked")
 
 to_block = random.sample(non_mutuals, nb)
 
@@ -45,14 +39,14 @@ print("List of people who will get blocked : ")
 for user in to_block :
     print(user.name)
 
-ready = input("Do you confirm you want to perform the mass block ? type \"y\" if yes : ")
+ready = input("Do you confirm you want to perform the mass softblock ? type \"y\" if yes : ")
 if ready == "y" :
     print("starting thanos-blocking ...")
     for user in to_block:
         print("soft-blocking user " + user.name + "; ID : " + str(user.id))
         softblock(user.id, user.name)
 
-print ("Done !")
-#output = softblock(sgt_bot_id, sgt_bot_name)
-#print(output)
+    print ("Done !")
+else:
+    print("No softblock performed")
 
