90b16ca73e698de3a040ca89f56790146078034d
Baptiste Colin <baptiste.colin@outlook.fr>
2022-07-30 18:41:50 +0200

    oopsi heehee

--- twithanos.py
+++ twithanos.py
@@ -4,10 +4,10 @@ import random
 
 credentials = json.loads(open("credentials").read())
 
-#api = twitter.Api(consumer_key=credentials["api_key"],
-#            consumer_secret=credentials["api_secret_key"],
-#            access_token_key=credentials["access_token"],
-#            access_token_secret=credentials["access_token_secret"])
+api = twitter.Api(consumer_key=credentials["api_key"],
+            consumer_secret=credentials["api_secret_key"],
+            access_token_key=credentials["access_token"],
+            access_token_secret=credentials["access_token_secret"])
 
 def softblock(user_id, screen_name):
     api.CreateBlock(user_id=user_id, screen_name=screen_name)
